6c473fad2cb0dfdb1a62e2f8343b829eb4cda76d
= <=>
2022-10-31 23:18:53 +0100

    new commit

--- src/componenets/ReposComponenet.jsx
+++ src/componenets/ReposComponenet.jsx
@@ -2,21 +2,27 @@ import React from "react";
 import { useState, useEffect,useContext} from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { ReposContext } from "../reposContext";
+import {FaLongArrowAltLeft} from 'react-icons/fa'
+
 
 function ReposComponenet() {
 
 
   const navigate = useNavigate();
   const {repos}=useContext(ReposContext)
+  console.log(repos);
 
   return (
     <div className="repos">
-      <button onClick={() => navigate(-1)}>back</button>
+      <button onClick={() => navigate(-1)}>
+        <FaLongArrowAltLeft />
+        
+      </button>
       {repos?.map((repo) => {
         return (
           <Link className="repo" to={repo.name}>
-            <h1>{repo.name}</h1>
-            <h4>{repo.owner.login}</h4>
+            <p className="name">{repo.name}</p>
+            <p className="description">{repo.description}</p>
             {/* <p className="description">{repo.description}</p> */}
             <p className="language">{repo.language}</p>
           </Link>
